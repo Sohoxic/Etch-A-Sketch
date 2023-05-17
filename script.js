3bf531ad618a7ab6623d17a45911447872621238
Sohoxic <pes2202100825@pesu.pes.edu>
2023-05-17 14:38:37 +0530

    tried adding a button to take the userinput for the size of the sketchpad

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 let container = document.createElement('div');
 container.classList.add('container');
 
-for(let i=0; i<16; i++){
+let button = document.createElement('button');
+button.classList.add('btn');
+button.textContent = "Click to enter No. of grid boxes you want for the sketchpad";
+
+let n;
+
+function noOfBox(n){
+  noOfBox = n;
+}
+button.addEventListener('click',() => {
+  n = prompt('Enter the grid size for the Sketchpad!');
+  noOfBox(n);
+});
+
+// console.log(noOfBox);
+
+for(let i=0; i<100000; i++){
     let childDiv = document.createElement('div');
     childDiv.classList.add('box1');
     container.appendChild(childDiv);
 }
 
+document.body.append(button);
 document.body.appendChild(container);
 
 for (let i = 0; i < container.children.length; i++) {
     let box = container.children[i];
     box.addEventListener('mouseover', function(e) {
-      e.target.style.backgroundColor = '#ACBCFF';
+      e.target.style.backgroundColor = '#D25380';
     });
   }
+
   
 
